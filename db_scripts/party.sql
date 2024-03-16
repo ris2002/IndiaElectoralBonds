8323888ae47067c0bbd2dd65cd5549507e982789
ramkipalle <ramki.palle@gmail.com>
2024-03-16 09:40:22 -0500

    Added table screation script files

---
+++ db_scripts/party.sql
@@ -0,0 +1,10 @@
+CREATE TABLE party (
+  id serial PRIMARY KEY,
+  party_name varchar(500) NOT NULL DEFAULT '',
+  encashment_date date,
+  denomination integer,
+  e_date varchar(50),
+  year smallint,
+  month smallint,
+  date smallint
+);
